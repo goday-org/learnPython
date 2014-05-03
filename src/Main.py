23add050d1d1a8a6d0ef1074f16bcc10c020e1a2
Andy <goday.org@gmail.com>
2014-05-03 16:45:36 +0800

    [Update] How to add comment

--- src/Main.py
+++ src/Main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-  
 '''
 Created on May 1, 2014
 
@@ -5,5 +6,11 @@ Created on May 1, 2014
 '''
 print 'Hello world'
 print range(10)
+
+def foo():
+    "This is a DOC comment"
+    print 'Try to use DOC comment'
+
 if __name__ == '__main__':
+    foo()
     pass
